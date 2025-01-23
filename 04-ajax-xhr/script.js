9ba8cec8ca827146f37b1a17701eb096c679de94
Daksh <daksh.prajapati@aliansoftware.com>
2025-01-23 13:26:47 +0530

    fatch data using apis

--- 04-ajax-xhr/script.js
+++ 04-ajax-xhr/script.js
@@ -1,2 +1,19 @@
-console.log("hello")
-console.log("hsdhdskdk")
+const xhr = new XMLHttpRequest();
+
+xhr.open('GET','https://api.github.com/users/Daksh1090/repos')
+
+xhr.onreadystatechange = function (){
+  if(this.readyState === 4 && this.status === 200 ){
+    const data = JSON.parse(this.responseText);
+    data.forEach((repo, index) => {
+      
+      console.log(index)
+        const li = document.createElement('li')
+      li.innerHTML = `<strong>${repo.name}</strong> - ${repo.description}`;
+      document.getElementById('results').appendChild(li)
+     
+    })
+  }
+}
+
+xhr.send();
